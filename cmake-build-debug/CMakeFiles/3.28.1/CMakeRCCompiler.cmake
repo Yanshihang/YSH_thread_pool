2171aa22e1a7d7eae555f7c68648894fd48504f4
Yanshihang <2495872823@qq.com>
2024-04-13 22:33:48 +0800

    重新理解线程池的实现并添加注释，同时修改逻辑错误

--- cmake-build-debug/CMakeFiles/3.28.1/CMakeRCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.28.1/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "D:/programming/CLion/CLion 2023.1.3/bin/mingw/bin/windres.exe")
+set(CMAKE_RC_COMPILER "C:/compiler/mingw64/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/CLionProject/new_thread_pool
+# Install script for directory: E:/CLionProject/YSH_thread_pool
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/programming/CLion/CLion 2023.1.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/compiler/mingw64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/CLionProject/new_thread_pool/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/CLionProject/YSH_thread_pool/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
